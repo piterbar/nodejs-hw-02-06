06acf8aa3b8fefd8a4b06ec26e970840c6cb3cc8
Piotr Baraniak <piterb86@gmail.com>
2024-02-09 09:23:36 +0100

    env update

--- database.js
+++ database.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const mongoose = require('mongoose');
-const dbUri = 'mongodb+srv://piterbartest:goit1234@cluster0.d55wcyi.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const dbUri = process.env.DB_URI;
 
 mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
